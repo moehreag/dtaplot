af991f85759ada7c2a2cc2324ef0421febc00e03
moehreag <moehreag@gmail.com>
2024-04-16 13:59:50 +0200

    add android module

---
+++ android/src/main/java/io/github/moehreag/dtaplot/android/ui/plot/DialogFragment.java
@@ -0,0 +1,4 @@
+package io.github.moehreag.dtaplot.android.ui.plot;
+
+public class DialogFragment {
+}
---
+++ android/src/main/java/io/github/moehreag/dtaplot/android/ui/plot/HomeViewModel.java
@@ -0,0 +1,12 @@
+package io.github.moehreag.dtaplot.android.ui.plot;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+
+
+	public HomeViewModel() {
+
+	}
+}
